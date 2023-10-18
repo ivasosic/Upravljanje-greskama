f453fd8373ca21945a79486c945bfe2732353b65
ivasosic <145933200+ivasosic@users.noreply.github.com>
2023-10-18 08:25:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljanje_greskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola=false;
+            do {
+                Console.WriteLine("Upisi svoju godinu rodenja: ");
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Roden si: " + godRod + " godine.");
+                    kontrola = true;    
+                }
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+                    kontrola=false;
+                }
+            }while (kontrola=true);
+            Console.ReadKey();  
+
+        }
+    }
+}
